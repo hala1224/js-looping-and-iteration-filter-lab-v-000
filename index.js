a493b5d93b320cfcbdb79a5f433a5eb2132e4d2a
Natalie Halawani <hala1224@gmail.com>
2018-10-17 22:55:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file
+function findMatching(arr, str) {
+  const result = arr.filter(name => name===str);
+  
+  for (const element of arr) {
+      
+        if (element.toUpperCase()===str.toUpperCase() && element!==str) {
+          result.push(element);
+        }
+      }
+      return result;
+}
+
+function fuzzyMatch(arr, str) {
+      result=[];
+      for (const element of arr) {
+         if(element.startsWith(str)) {
+           result.push(element);
+         }
+      }
+  return result;
+}
+
+function matchName(arr, str) {
+  result={};
+  for (const key in arr) {
+    if(arr[key]===str){
+      result << arr[key];
+    }
+  }
+  return result;
+}
